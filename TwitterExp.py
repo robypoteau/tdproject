f274cc916f7628403bbd3d310ab14629a4e20f66
Roby <rpoteau2010@my.fit.edu>
2013-09-28 01:45:45 -0400

    Add code, add graph

--- TwitterExp.py
+++ TwitterExp.py
@@ -7,7 +7,9 @@ access_token_secret = "Lk16RRBkhrNZoIuMZ23RVmKAldYSmzGQiYJeIEm4R0"
 
 issue = TwitterAPI(consumer_key, consumer_secret, access_token_key, access_token_secret)
 
-issue.request('search/tweets', {'q':'zzz'})
+issue.request('search/tweets', {'q':'Jesus', 'lang':'en', 'count':'10'})
 iteration = issue.get_iterator()
-for item in iteration:
-    print item
+iteration.keys()
+'''for item in iteration:
+    print item['text'] + item['text'] + '\n' 
+'''
