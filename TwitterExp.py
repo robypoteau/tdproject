35dfc6a188f80ce1dd97d8791e267b921a97d4d3
Roby <rpoteau2010@my.fit.edu>
2013-10-01 01:27:33 -0400

    Code addition:file

--- TwitterExp.py
+++ TwitterExp.py
@@ -7,10 +7,14 @@ access_token_secret = "Lk16RRBkhrNZoIuMZ23RVmKAldYSmzGQiYJeIEm4R0"
 
 issue = TwitterAPI(consumer_key, consumer_secret, access_token_key, access_token_secret)
 
-issue.request('search/tweets', {'q':'Jesus', 'lang':'en', 'count':'10'})
+#Contacts the twitter REST Api and asked for tweets satisfying the following criteria
+issue.request('search/tweets', {'q':'obama', 'geocode':'28.07925,-80.621903,5mi', 'lang':'en', 'count':'10'})
 iteration = issue.get_iterator()
 
-with open('twitterNodes.csv', 'w+') as afile:
+with open('C:/Users/Roby/Documents/Python/tdproject/twitterNodes.csv', 'a') as afile:
     for item in iteration:
-        afile.write(item['id_str'].encode('utf-8') +','+ item['text'].encode('utf-8') + '\n')
+        ident = item['id_str'].encode('utf-8')
+        tweet = item['text'].encode('utf-8')
+        tweet = " ".join(tweet.split()) #Removes the excess whitespace from the tweet
+        afile.write(ident +','+ tweet + '\n') #Writes the id and tweet to a file
     
