e93d353bb749579f4acffac353d53e49787f9f98
Roby <rpoteau2010@my.fit.edu>
2013-10-07 00:24:49 -0400

    Code for finding ngrams

--- TwitterExp.py
+++ TwitterExp.py
@@ -1,5 +1,6 @@
 from TwitterAPI import TwitterAPI
 import sys
+import time
 
 consumer_key = "7UhYU7wwZZAMQQnwperCUA"
 consumer_secret = "gADPNwkySetoudLGRhYDd6dmHgABbPZjuthX1xM4C6k"
@@ -9,28 +10,32 @@ access_token_key = "1587711043-x8cU7WbWjPBBXuwykoMgNwnpU6sO76BeBtHh7Aj"
 call = TwitterAPI(consumer_key, consumer_secret, access_token_key, access_token_secret)
 
 #Contacts the twitter REST Api and asked for tweets satisfying the following criteria
-try:
-    call.request('statuses/sample')
-except :
-    print 'Call fail'
-    sys.exit()
 
-response = call.get_iterator()
+finish_time = time.time() + 28800
+while(finish_time > time.time()): # for 8 hours
+    try:
+        call.request('statuses/sample')
+    except :
+        print 'Call fail'
 
-with open('C:/Users/Roby/Documents/Python/tdproject/twitterNodes2.tsv', 'a') as afile:
-    for item in response:
-        try:
-            lang = item[u'lang'].encode('utf-8')
-            if(lang == 'en'):
-                #print item[u'id_str'].encode('utf-8')
-                ident = item[u'id_str'].encode('utf-8')
-                tweet = item[u'text'].encode('utf-8')
-                tweet = " ".join(tweet.split()) #Removes the excess whitespace from the tweet
-                afile.write(lang + "\t" + ident +'\t'+ tweet + '\n') #Writes the id and tweet to a file
-        
-        except KeyError as ke:
-            continue
+    response = call.get_iterator()
+
+    with open('C:/Users/Roby/Documents/Python/tdproject/twitterNodes2.tsv', 'a') as afile:
+        for item in response:
+            try:
+                lang = item[u'lang'].encode('utf-8')
+                if(lang == 'en'):
+                    #print item[u'id_str'].encode('utf-8')
+                    ident = item[u'id_str'].encode('utf-8')
+                    tweet = item[u'text'].encode('utf-8')
+                    tweet = " ".join(tweet.split()) #Removes the excess whitespace from the tweet
+                    afile.write(lang + "\t" + ident +'\t'+ tweet + '\n') #Writes the id and tweet to a file
             
-        except:
-            print "Unexpected error:", sys.exc_info()[0]
-            continue
+            except KeyError as ke:
+                continue
+                
+            except:
+                print "Unexpected error:", sys.exc_info()[0]
+                continue
+time.sleep(1200)
+                
--- findNgrams.py
+++ findNgrams.py
@@ -1,8 +1,12 @@
 import nltk
 
 #Import File
+with open("C:\Users\Roby\Documents\Python\tdproject\twitter_raw.txt") as afile
+raw = afile.read()
+tokens = nltk.word_tokenize(raw)
 
 #Calculate n-grams
+nltk.bigrams(tokens)
 
 #find top five or 10
 
