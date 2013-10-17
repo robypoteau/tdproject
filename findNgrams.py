d83bc24649c262c5c3ab400265e9f10018a58119
Roby <rpoteau2010@my.fit.edu>
2013-10-17 15:29:22 -0400

    code for finding bigrams and frequency count

--- findNgrams.py
+++ findNgrams.py
@@ -1,12 +1,21 @@
 import nltk
 
 #Import File
-with open("C:\Users\Roby\Documents\Python\tdproject\twitter_raw.txt") as afile
-raw = afile.read()
-tokens = nltk.word_tokenize(raw)
+with open("C:/Users/Roby/Documents/Python/tdproject/twitter_raw.txt") as afile:
+    raw = afile.read()
+    tokens = nltk.word_tokenize(raw)
 
 #Calculate n-grams
-nltk.bigrams(tokens)
+twotuples = nltk.bigrams(tokens)
+#Remove the duplicates from the list
+twogram = list(set(twotuples))
 
 #find top five or 10
+freqTable = []
 
+for i in range(0,len(twogram)):
+    count = twotuples.count(twogram[i])
+    freqTable.append([count, twogram[i]])
+    
+freqTable.sort()
+freqTable.reverse()
