4c87447a5a16bb3802cca779a335f4a4124f6448
Roby <rpoteau2010@my.fit.edu>
2013-11-12 01:34:25 -0500

    updated

--- findSemSimilarity.py
+++ findSemSimilarity.py
@@ -1,5 +1,5 @@
-import nltk
-#import numpy as np
+import nltk, os
+import numpy as np
 from nltk.corpus import wordnet as wn
 wnl = nltk.stem.WordNetLemmatizer()
 
@@ -22,140 +22,233 @@ def rmv_contractions(token):
         token = "would"
     return token
 
-#def sem_similarity(sent1, sent2):
-sent1 = "\xe2\x80\x9c@itsss_ervin: I don't understand how I'm so bad at math and I'm Asian\xe2\x80\x9d \xf0\x9f\x98\x82\xf0\x9f\x98\x82\xf0\x9f\x98\x82\xf0\x9f\x91\x8f\xf0\x9f\x91\x8f\xf0\x9f\x91\x8f\xf0\x9f\x91\x8f\t\t\n"
-sent2 = '@saaaamanthaa_ samantha is soooo perf like omg \xf0\x9f\x98\xad\xf0\x9f\x98\xad\xf0\x9f\x98\xad\xf0\x9f\x92\x98\xf0\x9f\x92\x81\n'
-
-#Clean up text 
-sent1 = sent1.decode('ascii', 'ignore').encode('utf-8')
-sent2 = sent2.decode('ascii', 'ignore').encode('utf-8')
-
-#Tokenize text
-sent1 = nltk.tokenize.TreebankWordTokenizer().tokenize(sent1.lower())
-sent2 = nltk.tokenize.TreebankWordTokenizer().tokenize(sent2.lower())
-
-#Remove Contractions
-sent1 = [rmv_contractions(token) for token in sent1]
-
-#Stem text
-#porter = nltk.PorterStemmer()
-#stem1 = [porter.stem(t) for t in sent1]
-#stem2 = [porter.stem(t) for t in sent2]
-
-#Tag tokens with parts of speech
-sent1 = nltk.pos_tag(sent1)
-sent2 = nltk.pos_tag(sent2)
-
-#Word sense disambiguations
-word_sense1 = []
-word_sense2 = []
-
-for word in sent1:
-    a = (word[1].lower())[0]
-    if(a == 'n'):
-        ws = wn.synsets(wnl.lemmatize(word[0]), wn.NOUN)
-    elif(a == 'v'):
-        ws = wn.synsets(wnl.lemmatize(word[0]), wn.VERB)
-    elif(a == 'j'):
-        ws = wn.synsets(wnl.lemmatize(word[0]), wn.ADJ)
-    else:
-        ws = wn.synsets(wnl.lemmatize(word[0]))
-    if(ws != []):
-        word_sense1.append(ws)
-
-#Word sense disambiguagtion
-for word in sent2:
-    a = (word[1].lower())[0]
-    if(a == 'n'):
-        ws = wn.synsets(wnl.lemmatize(word[0]), wn.NOUN)
-    elif(a == 'v'):
-        ws = wn.synsets(wnl.lemmatize(word[0]), wn.VERB)
-    elif(a == 'j'):
-        ws = wn.synsets(wnl.lemmatize(word[0]), wn.ADJ)
-    else:
-        ws = wn.synsets(wnl.lemmatize(word[0]))
-    if(ws != []):
-        word_sense2.append(ws)
-
-#gloss matrix
-gloss1 = []
-for word in word_sense1:
-    element = []
-    for synonym in word:
-        defn = synonym.definition
-        '''
-        hype = synonym.hypernyms()
-        hype = [h.definition for h in hype]
-        hype = " ".join(hype)
-        
-        hypo = synonym.hyponyms()
-        hypo = [h.definition for h in hypo]
-        hypo = " ".join(hypo)
-        
-        element.append([defn, hype, hypo])
-        '''
-        element.append(defn)
-    gloss1.append(element)
-
-gloss2 = []
-for word in word_sense1:
-    element = []
-    for synonym in word:
-        defn = synonym.definition
-        
-        '''
-        hype = synonym.hypernyms()
-        hype = [h.definition for h in hype]
-        hype = " ".join(hype)
-        
-        hypo = synonym.hyponyms()
-        hypo = [h.definition for h in hypo]
-        hypo = " ".join(hypo)
-        
-        element.append([defn, hype, hypo])'''
-        element.append(defn)
-    gloss2.append(element)
+def sem_similarity(sent1, sent2):
+    #sent1 = "\xe2\x80\x9c@itsss_ervin: I don't understand how I'm so bad at math and I'm Asian\xe2\x80\x9d \xf0\x9f\x98\x82\xf0\x9f\x98\x82\xf0\x9f\x98\x82\xf0\x9f\x91\x8f\xf0\x9f\x91\x8f\xf0\x9f\x91\x8f\xf0\x9f\x91\x8f\t\t\n"
+    #sent2 = '@saaaamanthaa_ samantha is soooo perf like omg \xf0\x9f\x98\xad\xf0\x9f\x98\xad\xf0\x9f\x98\xad\xf0\x9f\x92\x98\xf0\x9f\x92\x81\n'
     
-
-
-end = len(word_sense1)
-
-if(end > 1):
-    for i in range(0,end):    
-        score = []
-        indices = []
-        j=k=0
-        if(i == 0):
-            #Compare first and second word
-            for wa in gloss1[i]:
-                j+=1
-                print j
-                for wb in gloss1[i+1]:
-                    k+=1
-                    print j, k
-                    score.append(compare(wa[0], wb[0]))
-                    indices.append([j,k])
+    #Clean up text 
+    sent1 = sent1.decode('ascii', 'ignore').encode('utf-8')
+    sent2 = sent2.decode('ascii', 'ignore').encode('utf-8')
     
-        elif(i == end-1):
-            #Compare last two words
-            for wa in gloss1[i-1]:
-                j+=1
-                for wb in gloss1[i]:
-                    k+=1
-                    score.append(compare(wa[0], wb[0]))
-                    indices.append([j,k])
+    #Tokenize text
+    sent1 = nltk.tokenize.TreebankWordTokenizer().tokenize(sent1.lower())
+    sent2 = nltk.tokenize.TreebankWordTokenizer().tokenize(sent2.lower())
+    
+    #Remove Contractions
+    sent1 = [rmv_contractions(token) for token in sent1]
+    sent2 = [rmv_contractions(token) for token in sent2]
+    #Stem text
+    #porter = nltk.PorterStemmer()
+    #stem1 = [porter.stem(t) for t in sent1]
+    #stem2 = [porter.stem(t) for t in sent2]
+    
+    #Tag tokens with parts of speech
+    sent1 = nltk.pos_tag(sent1)
+    sent2 = nltk.pos_tag(sent2)
+    
+    #Word sense disambiguations
+    word_sense1 = []
+    word_sense2 = []
+    
+    for word in sent1:
+        a = (word[1].lower())[0]
+        if(a == 'n'):
+            ws = wn.synsets(wnl.lemmatize(word[0]), wn.NOUN)
+        elif(a == 'v'):
+            ws = wn.synsets(wnl.lemmatize(word[0]), wn.VERB)
+        elif(a == 'j'):
+            ws = wn.synsets(wnl.lemmatize(word[0]), wn.ADJ)
         else:
-            #Compare middles words
-            for wa in gloss1[i]:
-                j+=1
-                for wb in gloss1[i+1]:
-                    k+=1
-                    score.append(compare(wa[0], wb[0]))
-                    indices.append(j)
-        mx = indices[score.index(max(score))]
-        word_sense1[i] = word_sense1[i][mx]
-#Create a similarity matrix
-#sim_mat = np.zeros(len(sent1),len(sent2))
+            ws = wn.synsets(wnl.lemmatize(word[0]))
+        if(ws != []):
+            word_sense1.append(ws)
+    
+    #Word sense disambiguagtion
+    for word in sent2:
+        a = (word[1].lower())[0]
+        if(a == 'n'):
+            ws = wn.synsets(wnl.lemmatize(word[0]), wn.NOUN)
+        elif(a == 'v'):
+            ws = wn.synsets(wnl.lemmatize(word[0]), wn.VERB)
+        elif(a == 'j'):
+            ws = wn.synsets(wnl.lemmatize(word[0]), wn.ADJ)
+        else:
+            ws = wn.synsets(wnl.lemmatize(word[0]))
+        if(ws != []):
+            word_sense2.append(ws)
+            
+    #gloss matrix
+    gloss1 = []
+    for word in word_sense1:
+        element = []
+        for synonym in word:
+            examples = " ".join(synonym.examples)
+            defn = synonym.definition + " " + examples
+            
+            '''
+            hype = synonym.hypernyms()
+            hype = [h.definition for h in hype]
+            hype = " ".join(hype)
+            
+            hypo = synonym.hyponyms()
+            hypo = [h.definition for h in hypo]
+            hypo = " ".join(hypo)
+            
+            element.append([defn, hype, hypo])
+            '''
+            element.append(defn)
+        gloss1.append(element)
+    
+    gloss2 = []
+    for word in word_sense2:
+        element = []
+        for synonym in word:
+            examples = " ".join(synonym.examples)
+            defn = synonym.definition + " " + examples
+            
+            '''
+            hype = synonym.hypernyms()
+            hype = [h.definition for h in hype]
+            hype = " ".join(hype)
+            
+            hypo = synonym.hyponyms()
+            hypo = [h.definition for h in hypo]
+            hypo = " ".join(hypo)
+            
+            element.append([defn, hype, hypo])'''
+            element.append(defn)
+        gloss2.append(element)
+    
+        
+    #Pick one synset out of them all to represent our word            
+    end = len(word_sense1)
+    if(end > 1):
+        for i in range(0,end):    
+            score = []
+            indices = []
+            j=0
+            
+            if(len(word_sense1[i]) > 1):
+                if(i == 0):
+                    #Compare first and second word
+                    for wa in gloss1[i]:
+                        j+=1
+                        k=0
+                        for wb in gloss1[i+1]:
+                            k+=1
+                            score.append(compare(wa, wb))
+                            indices.append(j)
+            
+                elif(i == end-1):
+                    #Compare last two words
+                    for wa in gloss1[i-1]:
+                        j+=1
+                        k=0
+                        for wb in gloss1[i]:
+                            k+=1
+                            score.append(compare(wa, wb))
+                            indices.append(k)
+                else:
+                    #Compare middles words
+                    for wa in gloss1[i]:
+                        j+=1
+                        k=0
+                        for wb in gloss1[i+1]:
+                            k+=1
+                            score.append(compare(wa, wb))
+                            indices.append(j)
+    
+                #return the j index
+                mx = indices[score.index(max(score))] - 1
+                word_sense1[i] = word_sense1[i][mx]
+            else:
+                word_sense1[i] = word_sense1[i][0]
+    else:
+        if(len(word_sense1[0]) > 1):
+            pick = [-1*len(word.name) for word in word_sense1[0]]
+            word_sense1[0] = word_sense1[0][pick.index(max(pick))]
+            
+    end = len(word_sense2)
+    if(end > 1):
+        for i in range(0,end):    
+            score = []
+            indices = []
+            j=0
+            
+            if(len(word_sense2[i]) > 1):
+                if(i == 0):
+                    #Compare first and second word
+                    for wa in gloss2[i]:
+                        j+=1
+                        k=0
+                        for wb in gloss2[i+1]:
+                            k+=1
+                            score.append(compare(wa, wb))
+                            indices.append(j)
+            
+                elif(i == end-1):
+                    #Compare last two words
+                    for wa in gloss2[i-1]:
+                        j+=1
+                        k=0
+                        for wb in gloss2[i]:
+                            k+=1
+                            score.append(compare(wa, wb))
+                            indices.append(k)
+                else:
+                    #Compare middles words
+                    for wa in gloss2[i]:
+                        j+=1
+                        k=0
+                        for wb in gloss2[i+1]:
+                            k+=1
+                            score.append(compare(wa, wb))
+                            indices.append(j)
+    
+                #return the j index
+                mx = indices[score.index(max(score))] - 1
+                word_sense2[i] = word_sense2[i][mx]
+            else:
+                word_sense2[i] = word_sense2[i][0]
+    else:
+        if(len(word_sense2[0]) > 1):
+            pick = [-1*len(word.name) for word in word_sense2[0]]
+            word_sense2[0] = word_sense2[0][pick.index(max(pick))]
+    
+    #Create a similarity matrix
+    sim_mat = []
+    for word1 in word_sense1:
+        for word2 in word_sense2:
+            if(word1.pos == word2.pos):
+                value = (word1.path_similarity(word2))
+                if(value == None):
+                    value =0
+            else:
+                value = 0
+            sim_mat.append(value)
+    
+    return 2*sum(sim_mat)/(len(word_sense1)+end)
 
-#Add to a file
-#return [word_sense1, sent2]
+#Import File
+raw=[]
+N = 10
+EdgeMatrix = np.zeros((N,N))
+thisDir = os.path.dirname(os.path.realpath(__file__))
+with open(os.path.join(thisDir, "twitter_raw.txt")) as afile:
+    for i in range(0,10):
+        raw.append(afile.readline())
+
+with open(os.path.join(thisDir, "edges.txt"), 'w') as afile:
+    afile.write(";")
+    for i in range(0,N):
+        afile.write("A"+str(i+1)+";")
+    for i in range(0,N):
+        for j in range(i,N):
+            if(i != j):
+                EdgeMatrix[i][j] = sem_similarity(raw[i], raw[j])
+                EdgeMatrix[j][i] = EdgeMatrix[i][j]
+            else:
+                EdgeMatrix[i][j] = 0.0
+
+
+    
